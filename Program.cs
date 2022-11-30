329e90d286f560c2b327cfd8e8c4268e220472d1
ns21-022 <ns21-022@DESKTOP-S0P8FQ1>
2022-11-30 22:05:18 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bill_Calculator
+{
+    internal class Customer
+    {
+        private string Firstname;
+        private string Lastname;
+        private double PlotNumber;
+        private string Location;
+        private int Usertype;
+        private double AmntUsed;
+
+
+
+    }
